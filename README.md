4f757a08bb77859306ea6847429e4ff6d17667d8
shiwani08 <shiwanisoni29082002@gmail.com>
2025-04-10 12:33:55 +0530

    README.md added

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a simple Slot Machine project made using Python. 
+
+It includes multiple funtions:
+    i. deposit() - to get the amount to be deposited
+    ii. get_num_of_lines() - to get the number of lines that is to be entered by the player
+    iii. get_bet() - to get the amount the player wants to bet on
+    
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# cap letter means constants
+MAX_BET = 10000
+MIN_BET = 10
+MAX_LINES = 3
+
 def deposit():
     # checking if the entered amount is a positive number
     while True: 
@@ -17,4 +22,25 @@ def deposit():
     
     return amount
 
-deposit()
+def get_num_of_lines(): 
+    while True: 
+        lines = (input("Enter the number of lines to bet on between (1-" + str(MAX_LINES) + ")! "))
+
+        try:
+            lines = int(lines)
+            if(1 <= lines <= MAX_LINES):
+                print("The lines that you have entered is: ", lines)
+                break
+            else:
+                print("Please enter between (1-" + str(MAX_LINES) + ")! ")
+
+        except ValueError:
+            print("Please enter a valid number!")
+
+    return lines
+
+def main():
+    amount = deposit()
+    lines = get_num_of_lines
+
+main()
