ec6fea0719249a0c3c32053a6db4cc75a4f82509
shiwani08 <shiwanisoni29082002@gmail.com>
2025-04-10 11:33:10 +0530

    deposit function added

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello")
+def deposit():
+    # checking if the entered amount is a positive number
+    while True: 
+        amount = input("How much amount would you like to deposit? ")
+
+        # checking if the entered string value is a digit
+        try:
+            amount = int(amount)
+            if(amount > 0):
+                print("The deposited amount is:",amount)
+                break
+            else:
+                print("Enter a positive amount!")
+
+        except ValueError:
+            print("Please enter a number!")
+    
+    return amount
+
+deposit()
